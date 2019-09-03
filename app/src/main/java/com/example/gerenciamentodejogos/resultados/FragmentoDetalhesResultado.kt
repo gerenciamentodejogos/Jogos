dd3d4323a6b7b003ab778f74fa487d2c1347f3f1
fabiorribeiro <wwwpontofabio@gmail.com>
2019-09-03 11:34:56 -0300

    Atalização de layout da tela de resultados

--- app/src/main/java/com/example/gerenciamentodejogos/resultados/FragmentoDetalhesResultado.kt
+++ app/src/main/java/com/example/gerenciamentodejogos/resultados/FragmentoDetalhesResultado.kt
@@ -3,33 +3,32 @@ package com.example.gerenciamentodejogos.resultados
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.content.res.ColorStateList
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.CardView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import com.example.gerenciamentodejogos.R
 
 import com.example.gerenciamentodejogos.dados.PROXIMO_CONCURSO
 import com.example.gerenciamentodejogos.modelos.*
 import com.example.gerenciamentodejogos.view_models.ResultadosViewModel
 import kotlinx.android.synthetic.main.fragmento_detalhes_resultado.*
-import java.sql.Time
+import kotlinx.android.synthetic.main.fragmento_princ_prox_jogo.*
 import java.text.SimpleDateFormat
-import java.util.*
 
 
 class FragmentoDetalhesResultado : Fragment() {
     private lateinit var VMResultados: ResultadosViewModel
 
     private var lineares: MutableList<LinearLayout> = mutableListOf()
-    private var dezenas: MutableList<TextView> = mutableListOf()
+    //private var dezenas: MutableList<TextView> = mutableListOf()
+    private var dezenas2: MutableList<CardView> = mutableListOf()
 
     private var tipoJogo: Int = 0
     private var numeroConcurso:Int = 1
@@ -67,12 +66,6 @@ class FragmentoDetalhesResultado : Fragment() {
             //VMResultados = ViewModelProviders.of(it)[ResultadosViewModel::class.java]
             val jogo = VMResultados.getJogo(tipoJogo, numeroConcurso)
             if (jogo == null) {
-                VMResultados.jogos.observe(it, Observer {jogos ->
-                    VMResultados.getJogo(tipoJogo, numeroConcurso)?.let {jogo ->
-                        atualizarDadosNaTela(jogo)
-                        VMResultados.jogos.removeObservers(it)
-                    }
-                })
                 DownloadTask().execute()
             } else {
                 atualizarDadosNaTela(jogo)
@@ -97,9 +90,11 @@ class FragmentoDetalhesResultado : Fragment() {
         val linhasNecessarias: Double// = 0.0
         val quantasLinhas: Int// = 0
 
-        textViewDezena = layoutInflater.inflate(R.layout.textview_dezena, linear,false) as TextView
+//        textViewDezena = layoutInflater.inflate(R.layout.textview_dezena, linear,false) as TextView
+        textViewDezena = layoutInflater.inflate(R.layout.textview_dezena2, linear,false) as CardView
         //textViewDezena.text = dezenas.count().plus(1).toString()
-        dezenasPorLinha = calcularDezenasQueCabem(linear, textViewDezena)
+//        dezenasPorLinha = calcularDezenasQueCabem(linear, textViewDezena)
+        dezenasPorLinha = 5
         linhasNecessarias = dezenasTotal.toDouble().div(dezenasPorLinha)
 
         quantasLinhas = if (linhasNecessarias - linhasNecessarias.toInt() != 0.0) {
@@ -115,7 +110,8 @@ class FragmentoDetalhesResultado : Fragment() {
         } else {
             dezenasLinhas.toInt()
         }
-        dezenas.add(textViewDezena)
+//        dezenas.add(textViewDezena)
+        dezenas2.add(textViewDezena)
 
         for (l in 2..quantasLinhas) {
             linear = layoutInflater.inflate(R.layout.linearlayout_dezenas, linearlayout_container_res, false) as LinearLayout
@@ -123,16 +119,16 @@ class FragmentoDetalhesResultado : Fragment() {
         }
 
         for (d in 2..dezenasTotal) {
-            textViewDezena = layoutInflater.inflate(R.layout.textview_dezena, linear, false) as TextView
-            //textViewDezena.text = dezenas.count().plus(2).toString()
-            dezenas.add(textViewDezena)
+            textViewDezena = layoutInflater.inflate(R.layout.textview_dezena2, linear, false) as CardView
+//            dezenas.add(textViewDezena)
+            dezenas2.add(textViewDezena)
         }
 
         var dezenasAdicionadas = 0
 
         for (lin in lineares) {
             for (d in 1..dezenasPorLinha) {
-                lin.addView(dezenas[dezenasAdicionadas++])
+                lin.addView(dezenas2[dezenasAdicionadas++])
                 if (dezenasAdicionadas == dezenasTotal) break
             }
         }
@@ -141,57 +137,63 @@ class FragmentoDetalhesResultado : Fragment() {
     private fun calcularDezenasQueCabem(linear: LinearLayout, textView: TextView): Int {
         //TODO - OBTER TAMANHO DA TELA E DOS ELEMENTOS LINEARLAYOUT E TEXTVIEW DAS DEZENAS
 
-        return 6
+        return 5
     }
 
     private fun atualizarDadosNaTela(jogo: Jogo) {
-        if (numeroConcurso == 2178) Log.d("TESTE", "COMEÇOU A MOSTRAR DADOS")
+        textView_ganhadores.setTextColor(jogo.corPrimaria)
+        textView_concurso.setTextColor(jogo.corPrimaria)
+
         //TODO -  RETIRAR '&& jogo.concurso < PROXIMO_CONCURSO[tipoJogo]'
         if (jogo.encerrado && jogo.concurso < PROXIMO_CONCURSO[tipoJogo]) {
             //adicionarDezenas()
 
-            if (jogo.acumulou) {
-                textView_acumulou.visibility = View.VISIBLE
-                textView_acumulou.setTextColor(jogo.corPrimaria)
-            }
 
-            if (jogo is MegaSena) {
-                for (d in 0..jogo.resultadoOrdenado.count() - 1) {
-                    val text = dezenas[d]
-                    text.text = jogo.resultadoOrdenado[d]
-                    text.background = resources.getDrawable(R.drawable.shape_redondo, null)
-                    text.backgroundTintList = ColorStateList.valueOf(jogo.corPrimaria)
-                }
+            textView_ganhadores.text = if (jogo.acumulou) {
+                 getText(R.string.texto_acumulou)
+            } else {
+                "${jogo.ganhadores.toString()} GANHADORES!"
             }
+            textView_ganhadores.visibility = View.VISIBLE
 
-            if (jogo is Quina) {
-                for (d in 0..jogo.resultadoOrdenado.count() - 1) {
-                    val text = dezenas[d]
-                    text.text = jogo.resultadoOrdenado[d]
-                    text.background = resources.getDrawable(R.drawable.shape_redondo, null)
-                    text.backgroundTintList = ColorStateList.valueOf(jogo.corPrimaria)
+            try {
+                if (jogo is MegaSena) {
+                    for (d in 0..jogo.resultadoOrdenado.count() - 1) {
+                        val text = dezenas2[d].findViewById<TextView>(R.id.tv_dezena)
+                        text.text = jogo.resultadoOrdenado[d]
+                        text.setBackgroundColor(jogo.corPrimaria)
+                    }
                 }
+
+                if (jogo is Quina) {
+                    for (d in 0..jogo.resultadoOrdenado.count() - 1) {
+                        val text = dezenas2[d].findViewById<TextView>(R.id.tv_dezena)
+                        text.text = jogo.resultadoOrdenado[d]
+                        text.setBackgroundColor(jogo.corPrimaria)
+                    }
+                }
+            } catch (erro: Exception) {
+
             }
 
             textView_concurso.text = jogo.concurso.toString()
             textView_data_sorteio.text = formatarData(jogo.dataConcurso)
-            textView_ganhadores.text = jogo.ganhadores.toString()
+            textView_local_sorteio.text = "${jogo.cidadeSorteio} - ${jogo.ufSorteio}\n${jogo.localSorteio}"
         } else {
             //CARREGAR INFORMAÇÕES DO PRÓXIMO CONCURSO
             textView_concurso.text = "PROXIMO CONCURSO"
             textView_data_sorteio.text = ""
-            textView_ganhadores.text = ""
+            textView_ganhadores.visibility = View.GONE
         }
 
         progressBar_detalhes_res.visibility = View.GONE
     }
 
-    inner class CriarDezenas: AsyncTask<Unit, Unit, Unit>() {
-        override fun doInBackground(vararg params: Unit?) {
-            criarDezenas()
-        }
-    }
-
+//    inner class CriarDezenas: AsyncTask<Unit, Unit, Unit>() {
+//        override fun doInBackground(vararg params: Unit?) {
+//            criarDezenas()
+//        }
+//    }
     inner class DownloadTask: AsyncTask<Unit, Unit, Jogo>() {
 
         override fun onPreExecute() {
@@ -216,6 +218,7 @@ class FragmentoDetalhesResultado : Fragment() {
         override fun onPostExecute(result: Jogo?) {
             result?.let {
                 VMResultados.adicionarJogo(it)
+                atualizarDadosNaTela(it)
             }
         }
     }
